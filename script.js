3e74a3563eac949857661192312d7762494f37f6
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2023-04-13 14:26:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your JS code here. If required.
-// function myFunction() {
-// 	var para=document.querySelector("#status");
-// 	para.innerHTML=`<h1>Entered Metaverse`;
-// 	// para.style.cssText = "font-size:55px;font-weight:bold;"	
-// }
-let btn=document.querySelector("#enterBtn");
-btn.addEventListener("click", ()=>{
+function myFunction() {
 	var para=document.querySelector("#status");
 	para.innerHTML=`<h1>Entered Metaverse`;
-})
+	// para.style.cssText = "font-size:55px;font-weight:bold;"	
+}
+// let btn=document.querySelector("#enterBtn");
+// btn.addEventListener("click", ()=>{
+// 	var para=document.querySelector("#status");
+// 	para.innerHTML=`<h1>Entered Metaverse`;
+// })
