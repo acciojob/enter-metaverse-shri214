78a5def1a1a453f65c2612c24d2de010c6db9840
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2023-04-13 13:52:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function myFunction() {
+	var para=document.querySelector("p");
+	para.innerHTML="Enterd Metaverse";
+	para.style.cssText = "font-size:55px;font-weight:bold;"	
+}
